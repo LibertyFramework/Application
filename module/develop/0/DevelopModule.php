357ef4b8f1fabc2e091a0211c53d39ef43207317
Francesco Bianco <info.francescobianco@gmail.com>
2016-01-25 11:35:13 +0100

    going to beta4

---
+++ module/develop/0/DevelopModule.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ *
+ *
+ */
+
+namespace Module\Develop;
+
+use Javanile\Liberty\Runtime;
+
+class DevelopModule
+{
+	/**
+	 *
+     *
+	 */
+	public function __construct()
+    {
+		$app = Runtime::getApp();
+	}
+}
