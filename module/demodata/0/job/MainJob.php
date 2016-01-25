179b3fb8654f3c39c19700a434a37dbdeb21f449
Francesco Bianco <info.francescobianco@gmail.com>
2016-01-25 11:32:22 +0100

    going to beta4

--- module/demodata/0/job/MainJob.php
+++ module/demodata/0/job/MainJob.php
@@ -11,10 +11,10 @@ class MainController {
 	 */
 	public function indexAction() {
 	
-		##
+		//
 		$app = App::getInstance();
 		
-		##
+		//
 		echo $app->ajax(array(
 			
 		));		
---
+++ module/demodata/1/code/MainCode.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * 
+ */
+namespace Module\Demodata\Code;
+
+/**
+ *
+ */
+use Javanile\Liberty\Runtime;
+
+/**
+ * main controlle class
+ */
+class MainCode
+{	
+	/**
+	 * 
+	 */
+	public function indexAction() {
+	
+		//
+		$app = Runtime::getApp();
+		
+		//
+		echo $app->html(array(
+			
+		));		
+	}	
+}
